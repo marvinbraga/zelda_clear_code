3e535a14ae8e295b1838e683f031b0f962b9aef2
Marcus Braga <mvbraga@gmail.com>
2022-03-01 19:32:49 -0300

    Creating the camera

--- code/player.py
+++ code/player.py
@@ -6,12 +6,17 @@ class Player(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
+        self.rect_undo = self.rect.copy()
 
-        self.direction = pygame.math.Vector2(pos)
-        print(self.direction, self.direction.x, self.direction.y)
+        self.direction = pygame.math.Vector2()
         self.speed = 5
 
+    def undo(self):
+        self.rect = self.rect_undo.copy()
+        return self
+
     def input(self):
+        self.rect_undo = self.rect.copy()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             self.direction.y = -1
@@ -28,7 +33,16 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 0
 
     def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
         self.rect.center += self.direction * speed
+
+    def is_tile_collision(self, group):
+        is_collide = pygame.sprite.spritecollideany(self, group)
+        if is_collide:
+            self.undo()
+        return is_collide
 
     def update(self):
         self.input()
