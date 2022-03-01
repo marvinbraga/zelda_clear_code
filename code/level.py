5c838b29f17b01b01df93c0ddf4c5b3cdbebb4e5
Marcus Braga <mvbraga@gmail.com>
2022-03-01 18:08:38 -0300

    player move

--- code/level.py
+++ code/level.py
@@ -9,6 +9,7 @@ class Level:
     def __init__(self):
 
         # get the display surface
+        self.player = None
         self.display_surface = pygame.display.get_surface()
 
         # sprite group setup
@@ -26,8 +27,9 @@ class Level:
                 if col == 'x':
                     Tile((x, y), *(self.visible_sprites, self.obstacle_sprites))
                 if col == 'p':
-                    Player((x, y), *(self.visible_sprites, ))
+                    self.player = Player((x, y), *(self.visible_sprites,))
 
     def run(self):
         # update and draw the game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
--- code/main.py
+++ code/main.py
@@ -19,7 +19,8 @@ class Game:
     def run(self):
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT or \
+                        (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                     pygame.quit()
                     sys.exit()
 
