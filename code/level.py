4167b0a52df3fcdb4f8b13a368f14021493fb9af
Marcus Braga <mvbraga@gmail.com>
2022-03-01 17:02:28 -0300

    Level Setup

---
+++ code/level.py
@@ -0,0 +1,33 @@
+import pygame
+
+from player import Player
+from settings import *
+from tile import Tile
+
+
+class Level:
+    def __init__(self):
+
+        # get the display surface
+        self.display_surface = pygame.display.get_surface()
+
+        # sprite group setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+        # sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILE_SIZE
+                y = row_index * TILE_SIZE
+                if col == 'x':
+                    Tile((x, y), *(self.visible_sprites, self.obstacle_sprites))
+                if col == 'p':
+                    Player((x, y), *(self.visible_sprites, ))
+
+    def run(self):
+        # update and draw the game
+        self.visible_sprites.draw(self.display_surface)
