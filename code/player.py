c6c9d63373c5539f7c25d698de3ddf85da9119f2
Marcus Braga <mvbraga@gmail.com>
2022-03-01 20:10:21 -0300

    Adjust camera move

--- code/player.py
+++ code/player.py
@@ -6,17 +6,21 @@ class Player(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
+        self.hit_box = self.rect.inflate(0, -26)
         self.rect_undo = self.rect.copy()
+        self.hit_box_undo = self.hit_box.copy()
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
 
     def undo(self):
         self.rect = self.rect_undo.copy()
+        self.hit_box = self.hit_box_undo.copy()
         return self
 
     def input(self):
         self.rect_undo = self.rect.copy()
+        self.hit_box_undo = self.hit_box.copy()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             self.direction.y = -1
@@ -36,12 +40,16 @@ class Player(pygame.sprite.Sprite):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
 
+        self.hit_box.x += self.direction.x * speed
+        self.hit_box.y += self.direction.y * speed
         self.rect.center += self.direction * speed
 
     def is_tile_collision(self, group):
-        is_collide = pygame.sprite.spritecollideany(self, group)
-        if is_collide:
-            self.undo()
+        is_collide = []
+        for sprite in group:
+            if sprite.hit_box.colliderect(self.hit_box):
+                is_collide.append(sprite)
+                self.undo()
         return is_collide
 
     def update(self):
